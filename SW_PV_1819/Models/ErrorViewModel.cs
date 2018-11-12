697b1276b8f68155d6e533b8d2ce8ff23955eaa7
Daniel Dias <daniel_._19@hotmail.com>
2018-11-12 22:20:04 +0000

    Add project files.

---
+++ SW_PV_1819/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SW_PV_1819.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
